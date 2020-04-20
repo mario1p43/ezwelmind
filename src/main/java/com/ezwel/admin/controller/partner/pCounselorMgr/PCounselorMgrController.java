75703a0b4f075c64cb2edddbc6f520b1271fdc66
TV 재훈 <mario1p43@gmail.com>
2020-04-20 18:14:05 +0900

    계속 첫회기 주호소 문제를 가져오는 문제 수정

--- src/main/java/com/ezwel/admin/controller/partner/pCounselorMgr/PCounselorMgrController.java
+++ src/main/java/com/ezwel/admin/controller/partner/pCounselorMgr/PCounselorMgrController.java
@@ -153,7 +153,7 @@ public class PCounselorMgrController extends commonController {
 				recordList.setSubjectItemCd1(firstRecordData.getSubjectItemCd1());
 				recordList.setSubjectItemCd2(firstRecordData.getSubjectItemCd2());
 				recordList.setSubjectItemCd3(firstRecordData.getSubjectItemCd3());
-				recordList.setMainIssue(firstRecordData.getMainIssue());
+				//recordList.setMainIssue(firstRecordData.getMainIssue());
 				
 				// 회기는 등록을 했으나 데이터가 없을 경우
 				if(firstRecordData.getSubjectItemCd1() == null || firstRecordData.getSubjectItemCd2() == null) {
